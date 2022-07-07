d8fbddfa543b00e1bff14a5c6a821898ba33c65e
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-07-07 10:16:49 +0100

    fixed bugs

--- server/main.lua
+++ server/main.lua
@@ -2,6 +2,9 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local PlayerJob = {}
 local PaymentTax = 15
 local Bail = {}
+local SellInkedBills = {
+    ["inked-bills"]  =  Config.InkedBillsValue ,
+}
 
 RegisterNetEvent('qb-gruppejob:server:PayBail', function(bool, vehInfo)
     local src = source
@@ -58,13 +61,41 @@ RegisterNetEvent('qb-gruppejob:server:DepositCase', function()
     Player.Functions.RemoveItem(item2, quantity)
 end)
 
-RegisterNetEvent('qb-gruppejob:server:InkedBillsReward', function()
+QBCore.Functions.CreateCallback('qb-gruppejob:server:get:LocalBillItem', function(source, cb)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local payment = Config.InkedBillsValue
+    local Ply = QBCore.Functions.GetPlayer(src)
+    local bill = Ply.Functions.GetItemByName("inked-bills")
+    if bill ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
 
-    Player.Functions.AddMoney("cash", payment)
-end) 
+RegisterNetEvent('qb-gruppejob:server:SellInkedBills', function()
+    local src = source
+    local price = 0
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    local xItem = Player.Functions.GetItemsByName(SellInkedBills)
+    if xItem ~= nil then
+        for k, v in pairs(Player.PlayerData.items) do
+            if Player.PlayerData.items[k] ~= nil then
+                if SellInkedBills[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (SellInkedBills[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+
+        Player.Functions.AddMoney("cash", price, "sold-bills")
+            TriggerClientEvent('QBCore:Notify', src, "You sold your Bills for $"..price)
+            TriggerEvent("qb-log:server:CreateLog", "sellinkedbill", "inkedbill", "blue", "**"..GetPlayerName(src) .. "** got $"..price.." for selling Inked Bills")
+                end
+            end
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, "You have no Bills..")
+    end
+
+end)
 
 RegisterNetEvent('qb-gruppejob:server:GivePayment', function()
     local src = source
